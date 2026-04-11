d340a65f9c4c430d883f27e3d2d87f23c0426563
kolli jyothika reddy <2400030366@kluniversity.in>
2026-04-11 18:25:36 +0530

    Update API URL to ngrok public backend URL

--- full-handloom-frontend/src/api/axiosInstance.js
+++ full-handloom-frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8080/api" });
+const api = axios.create({ baseURL: "https://quinton-uncogged-yoko.ngrok-free.dev/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
